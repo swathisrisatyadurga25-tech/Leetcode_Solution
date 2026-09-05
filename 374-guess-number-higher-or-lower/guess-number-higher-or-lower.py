852040d6aef050df9a523308d98bfe3d3d461599
Swathi Sri Satyadurga-147 <swathisrisatyadurga25@gmail.com>
2026-09-05 19:00:00 +0530

    Time: 43 ms (67.13%) | Memory: 19.2 MB (29.94%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
@@ -0,0 +1,14 @@
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left = 1
+        right = n
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if guess(mid) == 0:
+                return mid
+            elif guess(mid) == 1:
+                left = mid + 1
+            else:
+                right = mid - 1
